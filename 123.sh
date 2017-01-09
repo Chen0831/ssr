6bdb98dbccd61c2da0dc9c017b95ec951516ef71
未来 <aiastia@users.noreply.github.com>
2017-01-09 12:29:33 +0800

    Delete 123.sh

--- 123.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-wget https://github.com/shadowsocksr/shadowsocksr-csharp/releases/download/4.0.5/ShadowsocksR-4.0.5-win.7z
