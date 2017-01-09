55bc2c62776d8929a1eb5daa256c2588318fde8b
未来 <aiastia@users.noreply.github.com>
2017-01-09 12:50:39 +0800

    Add files via upload

---
+++ 123.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+
+
+			yum install -y git
+			
+			apt-get -y install git			
+
+
+git clone https://github.com/aiastia/123.git 
+
+cd ~/123/shadowsocks/shadowsocks
+
+chmod 777 run.sh
+
+./run.sh  
+
+echo "搞定了，玩的愉快 !"
+
+    echo "#############################################################"
+    echo "# Install ShadowsocksR                                      #"
+    echo "# Intro:                                                    #"
+    echo "# Author:                                                   #"
+    echo "# Github: https://github.com/aiastia/123                    #"
+    echo "#############################################################"
+    echo
+
+
+
