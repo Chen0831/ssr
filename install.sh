099f345853a3f05b85758da1a902eedfca1974c7
未来 <aiastia@users.noreply.github.com>
2017-01-11 12:39:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 			apt-get -y install git			
 
 
-git clone https://github.com/aiastia/123.git 
+git clone https://github.com/aiastia/ssr.git 
 
 cd ~/123/shadowsocks/shadowsocks
 
@@ -19,9 +19,9 @@ echo "搞定了，玩的愉快 !"
 
     echo "#############################################################"
     echo "# Install ShadowsocksR                                      #"
-    echo "# Intro:                                                    #"
-    echo "# Author:                                                   #"
-    echo "# Github: https://github.com/aiastia/123                    #"
+    echo "# Intro:    9345                                            #"
+    echo "# Author:    q4Gv9ImqX                                      #"
+    echo "# Github:   auth_sha1_v2_compatible  tls1.2_ticket_aut      #"
     echo "#############################################################"
     echo
 
